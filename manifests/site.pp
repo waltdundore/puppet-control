28652caae77412beee27ec9011ad80506db64e9c
Walt Dundore <walter.dundore@uga.edu>
2023-07-23 16:23:22 -0400

    docker

--- manifests/site.pp
+++ manifests/site.pp
@@ -12,7 +12,12 @@ package { 'git':
   ensure => installed,
 }
 
+
+###############
+# sudo config #
+###############
 include sudo
+
 sudo::conf { 'puppet_puppet':
     content  => 'puppet ALL=NOPASSWD: /usr/bin/puppet, /usr/local/bin/puppet',
   }
@@ -20,14 +25,24 @@ sudo::conf { 'vagrant':
   content => "Defaults:vagrant !requiretty\nvagrant ALL=(ALL) NOPASSWD: ALL",
 }
 
+#################
+# docker config #
+#################
+include 'docker'
+
+class { 'docker':
+  docker_users => ['wdundore'],
+}
+
+
 
 # default path for commands
 Exec { path => '/usr/bin:/bin:/usr/sbin:/sbin' }
 
 # by default all files overwritten by puppet
 # will be backed up on the puppetmaster
-filebucket { 'main':  server => 'puppet.apidb.org' }
-File { backup => main }
+#filebucket { 'main':  server => 'puppet.apidb.org' }
+#File { backup => main }
 
 # default permissions of 644 for files and 755 for directories
 File { mode => '0644' }
@@ -49,4 +64,8 @@ Mount {
 
 Package {
   allow_virtual => true,
+}
+
+package { 'git':
+  ensure => installed,
 }
